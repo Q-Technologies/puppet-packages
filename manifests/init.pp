4ac5e0b61b94c5f1f3736ffde1ce81a93c93859e
Matthew Mallard <matt@Q-Technologies.com.au>
2016-12-20 15:18:17 +1100

    Updated the hiera input to be a hash

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,16 +11,25 @@ class packages {
   }
   
   # Populate using hiera_hash command as we want to merge data from multiple hiera configs
-  $pkg_remove = hiera_array('packages::remove', [])
-  $pkg_add = hiera_array('packages::add',[])
+  $pkg_remove = hiera_hash('packages::remove', [])
+  $pkg_add = hiera_hash('packages::add',[])
+
+  $list_kernel_add = $pkg_add[$facts[kernel]]
+  $list_osfamily_add = $pkg_add[$facts[osfamily]]
+  $list_kernel_rm = $pkg_remove[$facts[kernel]]
+  $list_osfamily_rm = $pkg_remove[$facts[osfamily]]
+
+  $list_add = $list_kernel_add + $list_osfamily_add
+  $list_remove = $list_kernel_rm + $list_osfamily_rm
 
   # Make sure we don't try to remove any we have tried to add
-  $pkg_to_remove = $pkg_remove - $pkg_add
+  $pkg_to_remove = $list_remove - $list_add
   Package { $pkg_to_remove:
     ensure => absent,
   }
 
-  Package { $pkg_add:
+  $pkgs_to_add = $list_add
+  Package { $pkgs_to_add:
     ensure => installed,
   }
 
