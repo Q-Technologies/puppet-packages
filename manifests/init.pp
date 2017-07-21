4e57937a1db3bb92057cf4a1791e42feb23edf92
Matthew Mallard <matt@Q-Technologies.com.au>
2017-07-21 17:26:22 +1000

    Added some comments to the main manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,20 +7,22 @@ class packages {
   if $kernel == "Darwin" {
     Package { provider => 'macports' }
   }
-  
+
   if $osfamily == "Suse" {
     Package { provider => 'zypper' }
   }
-  
-  # Populate using hiera_hash command as we want to merge data from multiple hiera configs
-  $pkg_remove = hiera_hash('packages::remove', [])
-  $pkg_add = hiera_hash('packages::add',[])
 
+  # Populate using hiera_hash command as we want to merge data from multiple hiera configs
+  $pkg_remove = hiera_hash('packages::remove', {})
+  $pkg_add = hiera_hash('packages::add',{})
+
+  # get a list of packages from each subgroup from hiera
   $list_kernel_add = $pkg_add[$facts[kernel]]
   $list_osfamily_add = $pkg_add[$facts[osfamily]]
   $list_kernel_rm = $pkg_remove[$facts[kernel]]
   $list_osfamily_rm =  $pkg_remove[$facts[osfamily]]
 
+  # Merge these lists into one array
   if $list_osfamily_add {
     $list_add = any2array($list_kernel_add) + any2array($list_osfamily_add)
   } else {
